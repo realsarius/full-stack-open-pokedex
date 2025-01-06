5a16e1c797b19e329f586286051246f0fe13cc57
Berkan Sözer <berkansozer@outlook.com>
2025-01-06 11:39:31 +0300

    test: add new test for navigation to Ivysaur page and ability check

--- tests/e2e/test.e2e.spec.js
+++ tests/e2e/test.e2e.spec.js
@@ -8,4 +8,10 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible();
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible();
   });
+
+  test('navigate to Ivysaur page and check ability', async ({ page }) => {
+    await page.goto('/');
+    await page.click('text=ivysaur');
+    await expect(page.getByText('chlorophyll')).toBeVisible();
+  });
 });
