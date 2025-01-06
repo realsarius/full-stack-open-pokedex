7965024ce90e51461287f60b64167c06d1bb94e0
Berkan Sözer <berkansozer@outlook.com>
2025-01-06 10:54:07 +0300

    test: added Jest for unit testing and Playwright for e2e testing

---
+++ tests/test.spec.js
@@ -0,0 +1,11 @@
+const { test, describe, expect, beforeEach } = require('@playwright/test');
+
+// const baseUrl = 'http://localhost:8080';
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.getByText('ivysaur')).toBeVisible();
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible();
+  });
+});
