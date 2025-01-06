8ef688fe5dbc31a4af8481158ef21ded43007a25
Berkan Sözer <berkansozer@outlook.com>
2025-01-06 11:32:12 +0300

    fix: update jest config to match .jest.spec.jsx test files

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/tests/e2e/'],
-  testMatch: ['**/tests/unit/**/*.unit.test.js', '**/__tests__/**/*.unit.test.js'],
+  testMatch: ['**/tests/unit/**/*.jest.spec.jsx', '**/__tests__/**/*.jest.spec.jsx'],
 };
